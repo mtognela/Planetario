2f91501b4b415c387aa5ea7e6421c7f45898f1e7
Mattia Tognela <tognelamattia@protonmail.com>
2025-03-27 17:52:24 +0100

    Added sigleton

--- src/main/java/it/unibs/fp/Planetario/SistemaSolare/CorpoCeleste/Extend/Star.java
+++ src/main/java/it/unibs/fp/Planetario/SistemaSolare/CorpoCeleste/Extend/Star.java
@@ -3,22 +3,33 @@ package it.unibs.fp.Planetario.SistemaSolare.CorpoCeleste.Extend;
 import it.unibs.fp.Planetario.Data.Coordinate;
 import it.unibs.fp.Planetario.SistemaSolare.CorpoCeleste.CorpoCeleste;
 
+import java.util.ArrayList;
+
 public class Star extends CorpoCeleste {
     private static Star instance = null;
+    private ArrayList<Planet> listaPianeti = new ArrayList<>();
+    private String systemName;
 
-    private Star(double mass, String nome) {
+
+
+    private Star(double mass, String nome, ArrayList<Planet> listaPianeti, String systemName) {
         super(mass, new Coordinate(0, 0), nome);
+        this.listaPianeti = listaPianeti;
+        this.systemName = systemName;
     }
 
-    public static Star createInstance(double mass, String nome) {
-        if (instance == null) instance = new Star(mass, nome);
-        else throw new IllegalStateException("Star instance already created!");
+    public static Star createInstance(double mass, String nome, ArrayList<Planet> listaPianeti, String systemName) {
+        if (instance == null) instance = new Star(mass, nome, listaPianeti, systemName);
 
+        return instance;
+    }
+
+    public static Star getInstance() {
         return instance;
     }
 
     @Override
     public String toString() {
-        return super.toString();
+        return "FIX ME";
     }
 }
