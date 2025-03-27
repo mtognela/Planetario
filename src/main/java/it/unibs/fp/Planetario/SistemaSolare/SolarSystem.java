4dac2d02a77d0dfd9b35fdffb4f771b195fec843
Mattia Tognela <tognelamattia@protonmail.com>
2025-03-27 18:34:15 +0100

    added  method in SolarSystem createInstance getInstance getInstancePlanets getInstanceName

--- src/main/java/it/unibs/fp/Planetario/SistemaSolare/SolarSystem.java
+++ src/main/java/it/unibs/fp/Planetario/SistemaSolare/SolarSystem.java
@@ -4,9 +4,32 @@ import it.unibs.fp.Planetario.SistemaSolare.CorpoCeleste.Extend.*;
 import java.util.ArrayList;
 
 public class SolarSystem {
+    private static SolarSystem instance = null;
     ArrayList<Planet> planets;
-    ArrayList<Moon> moons;
     Star star;
+    String systemName;
 
+    private SolarSystem(Star star, ArrayList<Planet> planets, String systemName) {
+        this.star = star;
+        this.planets = planets;
+        this.systemName = systemName;
+    }
 
+    public static SolarSystem createInstance(Star star, ArrayList<Planet> listaPianeti, String systemName)  {
+        if (instance == null) instance = new SolarSystem(star, listaPianeti, systemName);
+
+        return instance;
+    }
+
+    public static SolarSystem getInstance() {
+        return instance;
+    }
+
+    public static ArrayList<Planet> getInstancePlanets() {
+        return instance.planets;
+    }
+
+    public static String getInstanceName() {
+        return instance.systemName;
+    }
 }
