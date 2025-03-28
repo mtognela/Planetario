28d65e50d9e2989780b5e8a04978d596ad2cfaf3
Mattia Tognela <tognelamattia@protonmail.com>
2025-03-28 19:16:17 +0100

    Added the metod that were in Bare.java in CorpoCeleste.java

--- src/main/java/it/unibs/fp/Planetario/SistemaSolare/CorpoCeleste/CorpoCeleste.java
+++ src/main/java/it/unibs/fp/Planetario/SistemaSolare/CorpoCeleste/CorpoCeleste.java
@@ -2,20 +2,41 @@ package it.unibs.fp.Planetario.SistemaSolare.CorpoCeleste;
 
 import it.unibs.fp.Planetario.Data.*;
 
-public class CorpoCeleste extends Bare {
-    protected String nome;
+public class CorpoCeleste {
+    private final String nome;
+    private double mass;
+    private final Coordinate coordinate;
+    private final int ID;
 
     public CorpoCeleste(double mass, Coordinate coordinate, String nome) {
-        super(mass, coordinate);
         this.nome = nome;
+        this.coordinate = coordinate;
+        this.ID = hashCode();
     }
 
     public String getName() {
         return nome;
     }
 
+    public double getMass() {
+        return mass;
+    }
+
+    public void updateMass(double update) {
+        this.mass += update;
+    }
+
+
+    public Coordinate getCoordinate() {
+        return coordinate;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
     @Override
     public String toString() {
-        return super.toString() + "Nome: " + nome;
+        return mass + " " + coordinate.toString() + " " + ID;
     }
 }
