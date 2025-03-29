773f7c1dc8317be0ae99847c55e2bfed4d67712f
Mattia Tognela <tognelamattia@protonmail.com>
2025-03-29 23:35:15 +0100

    dane a bunch of work in Io.java

--- src/main/java/it/unibs/fp/Planetario/Utils/Io.java
+++ src/main/java/it/unibs/fp/Planetario/Utils/Io.java
@@ -1,76 +1,170 @@
 package it.unibs.fp.Planetario.Utils;
+
 import com.kibo.pgar.lib.*;
+import it.unibs.fp.Planetario.Data.Coordinate;
 import it.unibs.fp.Planetario.SistemaSolare.CorpoCeleste.Extend.*;
 import it.unibs.fp.Planetario.SistemaSolare.SolarSystem;
 
-
-public class Io {
-    public static String GREETER = PrettyStrings.
-            prettify("Are you ready to crete your system?", AnsiColors.CYAN, AnsiWeights.BOLD, null);
-    public static String MESSARE_NAME_SYSTEM = PrettyStrings.
-            prettify("Input the System Name: ", AnsiColors.CYAN, AnsiWeights.BOLD, null);
-    public static String MESSAGE_MASS_STAR = PrettyStrings.
-            prettify("Input the Star mass: ", AnsiColors.CYAN, AnsiWeights.BOLD, null);
-    public static String MESSAGE_MASS_PLANET = PrettyStrings.
-            prettify("Input the Planet mass: ", AnsiColors.CYAN, AnsiWeights.BOLD, null);
-    public static String MESSAGE_NAME_STAR = PrettyStrings.
-            prettify("Input the Star Name: ", AnsiColors.CYAN, AnsiWeights.BOLD, null);
-    public static String MESSAGE_ADD_PLANET = PrettyStrings.
-            prettify("Do you want to add a planet to the system?", AnsiColors.CYAN, AnsiWeights.BOLD, null);
+public abstract class Io {
+    public static final String GREETER = PrettyStrings.prettify(
+            "Are you ready to create your system?", AnsiColors.CYAN, AnsiWeights.BOLD, null);
+    public static final String MESSAGE_NAME_SYSTEM = PrettyStrings.prettify(
+            "Input the System Name: ", AnsiColors.CYAN, AnsiWeights.BOLD, null);
+    public static final String MESSAGE_MASS_STAR = PrettyStrings.prettify(
+            "Input the Star mass: ", AnsiColors.CYAN, AnsiWeights.BOLD, null);
+    public static final String MESSAGE_NAME_STAR = PrettyStrings.prettify(
+            "Input the Star Name: ", AnsiColors.CYAN, AnsiWeights.BOLD, null);
 
     public static void setUpSystem() {
         if (InputData.readYesOrNo(GREETER)) {
-            String systemSystem = InputData.readStringNotEmpty(MESSARE_NAME_SYSTEM,true);
+            String systemName = InputData.readStringNotEmpty(MESSAGE_NAME_SYSTEM, true);
             Star.createInstance(
                     InputData.readDoubleWithMinimum(MESSAGE_MASS_STAR, 0),
-                    InputData.readStringNotEmpty(MESSAGE_NAME_STAR, true));
+                    InputData.readStringNotEmpty(MESSAGE_NAME_STAR, true)
+            );
 
-            SolarSystem.createInstance(Star.getInstance(), systemSystem);
+            SolarSystem.createInstance(Star.getInstance(), systemName);
 
-            Menu();
-
+            showMenu();
         } else {
+            System.out.println("Goodbye!");
             System.exit(0);
         }
     }
 
-    private  static void Menu() {
-        int scelta;
+    private static void showMenu() {
+        String[] options = {
+                "Aggiungi un pianeta",
+                "Aggiungi una luna",
+                "Rimuovi un pianeta",
+                "Rimuovi una luna",
+                "Mostra il sistema solare",
+                "Calcola centro di massa",
+        };
+
+        Menu menu = new Menu("System Menu", options, true, true, true);
+
+        int choice;
+
         do {
-            System.out.println("\nMenu: ");
-            System.out.println("1. Aggiungi un pianeta");
-            System.out.println("2. Aggiungi una luna");
-            System.out.println("3. Rimuovi un pianeta");
-            System.out.println("4. Rimuovi una luna");
-            System.out.println("5. Mostra il sistema solare");
-            System.out.println("6. Esci");
+            choice = menu.choose();
 
-            scelta = InputData.readIntegerBetween("Scegli un'opzione: ", 1, 6);
-
-
-            switch (scelta) {
+            switch (choice) {
                 case 1:
-                    SolarSystem.addPlanet();
+                    addPlanet();
                     break;
                 case 2:
-                    SolarSystem.removePlanet();
+                    addMoon();
                     break;
                 case 3:
-                    SolarSystem.addMoon();
+                    removePlanet();
                     break;
                 case 4:
-                    SolarSystem.removeMoon();
+                    removeMoon();
                     break;
                 case 5:
                     SolarSystem.showSolarSystem();
                     break;
                 case 6:
-                    System.out.println("Uscita in corso...");
+                    cmd();
                     break;
             }
-
-
-        } while (scelta != 6);
+        } while (choice != 0);
     }
 
+    private static void addPlanet() {
+        System.out.println("Aggiunta di un pianeta...");
+        String planetName = InputData.readStringNotEmpty("Nome del pianeta: ", true);
+        double mass = InputData.readDoubleWithMinimum("Massa del pianeta: ", 0);
+
+        double radius = InputData.readDoubleWithMinimum("Distanza dal sole (raggio): ", 0);
+        double theta = InputData.readDouble("Angolo theta (in gradi): ");
+
+        SolarSystem.addPlanet(mass, radius, theta, planetName);
+
+        System.out.println("Pianeta " + planetName + " aggiunto con successo!");
+    }
+
+    private static void addMoon() {
+        System.out.println("Aggiunta di una luna...");
+
+        // Trova il pianeta di riferimento
+        Planet planet = Search.searchPlanet(
+                InputData.readInteger("ID pianeta: "),
+                InputData.readStringNotEmpty("Nome pianeta: ", true)
+        );
+
+        if (planet == null) {
+            System.out.println("Errore: Pianeta non trovato.");
+            return;
+        }
+
+        String moonName = InputData.readStringNotEmpty("Nome della luna: ", true);
+        double moonMass = InputData.readDoubleWithMinimum("Massa della luna: ", 0);
+
+        // Input per coordinate polari
+        double radius = InputData.readDoubleWithMinimum("Distanza dal pianeta (raggio): ", 0);
+        double theta = InputData.readDouble("Angolo theta (in gradi): ");
+
+        // Creazione coordinate polari
+        Coordinate coordinate = new Coordinate(radius, theta);
+
+        Moon moon = new Moon(moonMass, coordinate, moonName, planet);
+        planet.addMoon(moon);
+
+        System.out.println("Luna " + moonName + " aggiunta con successo a " + planet.getName() + "!");
+    }
+
+    private static void removePlanet() {
+        System.out.println("Rimozione di un pianeta...");
+        Planet planet = Search.searchPlanet(
+                InputData.readInteger("ID pianeta: "),
+                InputData.readStringNotEmpty("Nome pianeta: ", true)
+        );
+
+        if (planet == null) {
+            System.out.println("Errore: Pianeta non trovato.");
+            return;
+        }
+
+        SolarSystem.removePlanet(planet);
+        System.out.println("Pianeta rimosso con successo.");
+    }
+
+    private static void removeMoon() {
+        System.out.println("Rimozione di una luna...");
+        Planet planet = Search.searchPlanet(
+                InputData.readInteger("ID pianeta: "),
+                InputData.readStringNotEmpty("Nome pianeta: ", true)
+        );
+
+        if (planet == null) {
+            System.out.println("Errore: Pianeta non trovato.");
+            return;
+        }
+
+        Moon moon = Search.searchMoonByPlanet(
+                planet,
+                InputData.readInteger("ID luna: "),
+                InputData.readStringNotEmpty("Nome luna: ", true)
+        );
+
+        if (moon == null) {
+            System.out.println("Errore: Luna non trovata.");
+            return;
+        }
+
+        SolarSystem.removeMoon(planet, moon);
+        System.out.println("Luna rimossa con successo.");
+    }
+
+    private static void cmd(){
+        Coordinate cmd = SolarSystem.calcCDM();
+        if (cmd == null) {
+            System.out.println("Ahhhh volevi");
+        }
+        else {
+            System.out.println("cmd " + cmd.toString());
+        }
+    }
 }
