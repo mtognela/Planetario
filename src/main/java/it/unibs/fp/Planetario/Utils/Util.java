0ff397e880f0c8a257854265b1fae7fd4c0703ce
Mattia Tognela <tognelamattia@protonmail.com>
2025-03-29 23:24:58 +0100

    deleted Util.java

--- src/main/java/it/unibs/fp/Planetario/Utils/Util.java
+++
@@ -1,9 +0,0 @@
-package it.unibs.fp.Planetario.Utils;
-
-import it.unibs.fp.Planetario.Data.Coordinate;
-import it.unibs.fp.Planetario.SistemaSolare.CorpoCeleste.Extend.*;
-import java.util.ArrayList;
-
-public class Util {
-
-}
