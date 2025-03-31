0a6854ed2b0e603de502bed4f225481445960e48
Mattia Tognela <tognelamattia@protonmail.com>
2025-03-31 21:23:29 +0200

    add BornOut.BuildPathFromTo BornOut.fromTo

--- src/main/java/org/Arch/IsTheBest/Planetarium/Utils/Io/BornOut.java
+++ src/main/java/org/Arch/IsTheBest/Planetarium/Utils/Io/BornOut.java
@@ -1,6 +1,15 @@
 package org.Arch.IsTheBest.Planetarium.Utils.Io;
 
+import org.Arch.IsTheBest.Planetarium.System.CorpoCeleste.Extend.Planet;
+
 public abstract class BornOut {
 
+    public static void fromTo() {
+
+    }
+
+    public static void BuildPathFromTo() {
+
+    }
 }
 
