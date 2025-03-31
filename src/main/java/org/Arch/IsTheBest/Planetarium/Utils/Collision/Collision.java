a054b7eaf8450698e4694d067924d75795ba27b9
Mattia Tognela <tognelamattia@protonmail.com>
2025-03-31 22:03:32 +0200

    work out some stuff

--- src/main/java/org/Arch/IsTheBest/Planetarium/Utils/Collision/Collision.java
+++ src/main/java/org/Arch/IsTheBest/Planetarium/Utils/Collision/Collision.java
@@ -5,6 +5,7 @@ import org.Arch.IsTheBest.Planetarium.System.CorpoCeleste.Extend.Moon;
 import org.Arch.IsTheBest.Planetarium.System.CorpoCeleste.Extend.Planet;
 import org.Arch.IsTheBest.Planetarium.System.OrbitingSystem;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public abstract class Collision {
     public static boolean isCoordinateUnique(ArrayList<Planet> planets, Coordinate coordinate) {
@@ -44,43 +45,46 @@ public abstract class Collision {
         ArrayList<Planet> planets = OrbitingSystem.getInstancePlanets();
         ArrayList<Moon> moons = OrbitingSystem.getMoons();
 
+        HashSet<Moon> removeMoons = new HashSet<>();
+        HashSet<Planet> removePlanets = new HashSet<>();
+
         for (int i = 0; i < planets.size(); i++) {
             for (int j=i+1; j<planets.size(); j++) {
                 if (detectCollision(planets.get(i), planets.get(j))) {
-                    OrbitingSystem.removePlanet(planets.get(i));
-                    OrbitingSystem.removePlanet(planets.get(j));
+                    removePlanets.add(planets.get(i));
+                    removeMoons.add(moons.get(j));
                 }
             }
         }
 
         for (int k = 0; k < moons.size(); k++) {
-            for (int l = k+1; l < planets.size(); l++) {
+            for (int l = k+1; l < moons.size() && moons.get(k).getPianetaRif().getID() != moons.get(l).getPianetaRif().getID(); l++) {
                 if (detectCollision(moons.get(k), moons.get(l))) {
-                    OrbitingSystem.removeMoon(moons.get(k));
-                    OrbitingSystem.removeMoon(moons.get(l));
+                    removePlanets.add(planets.get(k));
+                    removeMoons.add(moons.get(l));
                 }
             }
         }
 
         for (Planet planet : planets) {
-            for (Moon moon : moons) {
-                if (detectCollision(planet, moon)) {
-                    OrbitingSystem.removePlanet(planet);
-                    OrbitingSystem.removeMoon(moon);
+            for (int i = 0; i < moons.size() && planet.getID() != moons.get(i).getPianetaRif().getID(); i++ ) {
+                if (detectCollision(planet, moons.get(i))) {
+                    removePlanets.add(planet);
+                    removeMoons.add(moons.get(i));
                 }
             }
         }
     }
 
     private static boolean detectCollision(Planet p1, Planet p2) {
-        return p1.getCoordinate().distanceFrom(p2.getCoordinate()) < (p1.getCoordinate().getRADIUS() + p2.getCoordinate().getRADIUS());
+        return p1.getCoordinate().distanceFrom(p2.getCoordinate()) > (p1.getCoordinate().getRADIUS() + p2.getCoordinate().getRADIUS());
     }
 
     private static boolean detectCollision(Moon m1, Moon m2) {
-        return m1.getCoordinate().distanceFrom(m2.getCoordinate()) <  (m1.getCoordinate().getRADIUS() + m2.getCoordinate().getRADIUS());
+        return m1.getCoordinate().distanceFrom(m2.getCoordinate()) > (m1.getCoordinate().getRADIUS() + m2.getCoordinate().getRADIUS());
     }
 
     private static boolean detectCollision(Planet p, Moon m) {
-        return p.getCoordinate().distanceFrom(m.getCoordinate()) <  (p.getCoordinate().getRADIUS() + m.getCoordinate().getRADIUS());
+        return p.getCoordinate().distanceFrom(m.getCoordinate()) > (p.getCoordinate().getRADIUS() + m.getCoordinate().getRADIUS());
     }
 }
