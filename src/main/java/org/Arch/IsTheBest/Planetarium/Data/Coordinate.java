25d71986b76e04b03081bc3afb05353252316601
Mattia Tognela <tognelamattia@protonmail.com>
2025-04-01 19:28:43 +0200

    toString

--- src/main/java/org/Arch/IsTheBest/Planetarium/Data/Coordinate.java
+++ src/main/java/org/Arch/IsTheBest/Planetarium/Data/Coordinate.java
@@ -67,6 +67,6 @@ public class Coordinate {
 
     @Override
     public String toString() {
-        return PrettyStrings.prettify(String.format("{ x = %f, y = %f }",  X, Y), AnsiColors.CYAN, null,  null);
+        return String.format("{ x = %f, y = %f }",  X, Y);
     }
 }
