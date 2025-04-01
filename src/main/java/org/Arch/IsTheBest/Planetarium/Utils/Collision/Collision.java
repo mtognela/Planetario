e0354bf78a54d6ac142b0906af63a26d1e182939
Mattia Tognela <tognelamattia@protonmail.com>
2025-04-01 11:54:57 +0200

    done some fix

--- src/main/java/org/Arch/IsTheBest/Planetarium/Utils/Collision/Collision.java
+++ src/main/java/org/Arch/IsTheBest/Planetarium/Utils/Collision/Collision.java
@@ -12,41 +12,41 @@ public abstract class Collision {
         for (Planet planet : planets) {
             for (Moon moon : planet.getMoons()) {
                 if (planet.getCoordinate().equals(coordinate) || moon.getCoordinate().equals(coordinate)) {
-                    return true;
+                    return false;
                 }
             }
         }
-        return false;
+        return true;
     }
 
     public static boolean isCoordinateUnique(ArrayList<Planet> planets, Planet planetInput) {
         for (Planet planet : planets) {
             for (Moon moon : planet.getMoons()) {
                 if (planet.getCoordinate().equals(planetInput.getCoordinate()) || moon.getCoordinate().equals(planetInput.getCoordinate())) {
-                    return true;
+                    return false;
                 }
             }
         }
-        return false;
+        return true;
     }
 
     public static boolean isCoordinateUnique(ArrayList<Planet> planets, Moon moon) {
         for (Planet planet : planets) {
             for (Moon moons : planet.getMoons()) {
                 if (planet.getCoordinate().equals(moon.getCoordinate()) || moons.getCoordinate().equals(moon.getCoordinate())) {
-                    return true;
+                    return false;
                 }
             }
         }
-        return false;
+        return true;
     }
 
     public static void checkCollisions() {
         ArrayList<Planet> planets = OrbitingSystem.getInstancePlanets();
         ArrayList<Moon> moons = OrbitingSystem.getMoons();
 
-        HashSet<Moon> removeMoons = new HashSet<>();
-        HashSet<Planet> removePlanets = new HashSet<>();
+        ArrayList<Moon> removeMoons = new ArrayList<>();
+        ArrayList<Planet> removePlanets = new ArrayList<>();
 
         for (int i = 0; i < planets.size(); i++) {
             for (int j=i+1; j<planets.size(); j++) {
@@ -74,17 +74,25 @@ public abstract class Collision {
                 }
             }
         }
+
+        for (Moon removeMoon : removeMoons) {
+            OrbitingSystem.removeMoon(removeMoon);
+        }
+
+        for (Planet removePlanet : removePlanets) {
+            OrbitingSystem.removePlanet(removePlanet);
+        }
     }
 
     private static boolean detectCollision(Planet p1, Planet p2) {
-        return p1.getCoordinate().distanceFrom(p2.getCoordinate()) > (p1.getCoordinate().getRADIUS() + p2.getCoordinate().getRADIUS());
+        return p1.distanceFrom(p2.getCoordinate()) > (p1.getRADIUS() + p2.getRADIUS());
     }
 
     private static boolean detectCollision(Moon m1, Moon m2) {
-        return m1.getCoordinate().distanceFrom(m2.getCoordinate()) > (m1.getCoordinate().getRADIUS() + m2.getCoordinate().getRADIUS());
+        return m1.distanceFrom(m2.getCoordinate()) > (m1.getRADIUS() + m2.getRADIUS());
     }
 
     private static boolean detectCollision(Planet p, Moon m) {
-        return p.getCoordinate().distanceFrom(m.getCoordinate()) > (p.getCoordinate().getRADIUS() + m.getCoordinate().getRADIUS());
+        return p.distanceFrom(m.getCoordinate()) > (p.getRADIUS() + m.getRADIUS());
     }
 }
