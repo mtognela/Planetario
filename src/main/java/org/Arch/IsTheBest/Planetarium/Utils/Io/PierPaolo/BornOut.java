92377264ce29a9e0b1483d3caad36ee933e9a4db
Mattia Tognela <tognelamattia@protonmail.com>
2025-04-02 03:13:46 +0200

    refactor the code

--- src/main/java/org/Arch/IsTheBest/Planetarium/Utils/Io/PierPaolo/BornOut.java
+++ src/main/java/org/Arch/IsTheBest/Planetarium/Utils/Io/PierPaolo/BornOut.java
@@ -36,75 +36,36 @@ public abstract class BornOut {
         ArrayList<Moon> moons = OrbitingSystem.getMoons();
 
         if (InputData.readYesOrNo("Do you want to do a mix research?")) {
-            Planet planet;
-            Moon moon;
+            Planet planet = null;
+            Moon moon = null;
             do {
-                int id = Integer.parseInt(InputData.readString("Please enter an ID: ", true));
-                String nome = InputData.readString("Please enter a name ", true);
-
-                planet = Search.searchPlanet(id, nome);
-                moon = Search.searchMoon(id, nome);
-
-                if (planet == null && moon == null) System.out.println(NO_PLANET_FOUND);
-                else if (planet == null) System.out.println(moon);
-                else if (moon == null) System.out.println(planet);
-                else {
-                    System.out.println(moon);
-                    System.out.println(planet);
-                }
-            } while((planet != null || moon != null) && !InputData.readYesOrNo(DO_YOU_WANT_TO_SEARCH_AGAIN));
-
-            if (planet == null) {
-                do {
-                    int id = Integer.parseInt(InputData.readString("Please enter an ID: ", true));
-                    String nome = InputData.readString("Please enter a name ", true);
-                    planet = Search.searchPlanet(id, nome);
-                } while((planet != null) && !InputData.readYesOrNo(DO_YOU_WANT_TO_SEARCH_AGAIN));
-            } else if (moon == null) {
-                do {
-                    int id = Integer.parseInt(InputData.readString("Please enter an ID: ", true));
-                    String nome = InputData.readString("Please enter a name ", true);
-                    moon = Search.searchMoon(id, nome);
-                } while((moon != null) && !InputData.readYesOrNo(DO_YOU_WANT_TO_SEARCH_AGAIN));
-            }
+                planet = getPlanet(planet);
+                moon = getMoon(moon);
+            } while(!InputData.readYesOrNo(DO_YOU_WANT_TO_SEARCH_AGAIN));
 
             assert moon != null;
             assert planet != null;
 
             return fromTo(moon, planet);
         } else if (InputData.readYesOrNo("Do you want to search planets or moons (Y for Planet and n for moon)?")) {
-            Planet planet1, planet2;
+            Planet planet1 = null;
+            Planet planet2 = null;
 
-            do {
-                int id1 = Integer.parseInt(InputData.readString("Please enter an ID: ", true));
-                String nome1 = InputData.readString("Please enter a name ", true);
-                planet1 = Search.searchPlanet(id1, nome1);
-            } while((planet1 != null) && !InputData.readYesOrNo(DO_YOU_WANT_TO_SEARCH_AGAIN));
+            planet1 = getPlanet(planet1);
 
-            do {
-                int id2 = Integer.parseInt(InputData.readString("Please enter an ID: ", true));
-                String nome2 = InputData.readString("Please enter a name ", true);
-                planet2 = Search.searchPlanet(id2, nome2);
-            } while((planet2 != null) && !InputData.readYesOrNo(DO_YOU_WANT_TO_SEARCH_AGAIN));
+            planet2 = getPlanet(planet2);
 
             assert planet1 != null;
             assert planet2 != null;
 
             return fromTo(planet1, planet2);
         } else {
-            Moon moon1, moon2;
+            Moon moon1 = null;
+            Moon moon2 = null;
 
-            do {
-                int id1 = Integer.parseInt(InputData.readString("Please enter an ID: ", true));
-                String nome1 = InputData.readString("Please enter a name ", true);
-                moon1 = Search.searchMoon(id1, nome1);
-            } while((moon1 != null) && !InputData.readYesOrNo(DO_YOU_WANT_TO_SEARCH_AGAIN));
+            moon1 = getMoon(moon1);
 
-            do {
-                int id2 = Integer.parseInt(InputData.readString("Please enter an ID: ", true));
-                String nome2 = InputData.readString("Please enter a name ", true);
-                moon2 = Search.searchMoon(id2, nome2);
-            } while((moon2 != null) && !InputData.readYesOrNo(DO_YOU_WANT_TO_SEARCH_AGAIN));
+            moon2 = getMoon(moon2);
 
             assert moon1 != null;
             assert moon2 != null;
@@ -113,80 +74,64 @@ public abstract class BornOut {
         }
     }
 
+    private static Moon getMoon(Moon moon2) {
+        do {
+            int id2 = Integer.parseInt(InputData.readString("Please enter an Moon ID: ", true));
+            String nome2 = InputData.readString("Please enter a Moon name ", true);
+            moon2 = Search.searchMoon(id2, nome2);
+
+            System.out.println(moon2);
+        } while ((moon2 != null) && !InputData.readYesOrNo(DO_YOU_WANT_TO_SEARCH_AGAIN));
+
+        return moon2;
+    }
+
+    private static Planet getPlanet(Planet planet2) {
+        do {
+            int id2 = Integer.parseInt(InputData.readString("Please enter an Planet ID: ", true));
+            String nome2 = InputData.readString("Please enter a Planet name ", true);
+            planet2 = Search.searchPlanet(id2, nome2);
+
+            System.out.println(planet2);
+        } while ((planet2 != null) && !InputData.readYesOrNo(DO_YOU_WANT_TO_SEARCH_AGAIN));
+
+        return planet2;
+    }
+
     private static String rebusBuildPathFromTo() {
         ArrayList<Planet> planets = OrbitingSystem.getPlanets();
         ArrayList<Moon> moons = OrbitingSystem.getMoons();
-        Planet planetToSearch;
-        Moon moonToSearch;
 
         if (InputData.readYesOrNo("Do you want to do a mix research?")) {
+            Planet planet = null;
+            Moon moon = null;
+
             do {
-                int id = Integer.parseInt(InputData.readString("Please enter an ID: ", true));
-                String nome = InputData.readString("Please enter a name ", true);
+                planet = getPlanet(planet);
+                moon = getMoon(moon);
+            } while(!InputData.readYesOrNo(DO_YOU_WANT_TO_SEARCH_AGAIN));
 
-                planetToSearch = Search.searchPlanet(id, nome);
-                moonToSearch = Search.searchMoon(id, nome);
+            assert moon != null;
+            assert planet != null;
 
-                if (planetToSearch == null && moonToSearch == null) System.out.println(NO_PLANET_FOUND);
-                else if (planetToSearch == null) System.out.println(moonToSearch);
-                else if (moonToSearch == null) System.out.println(planetToSearch);
-                else {
-                    System.out.println(moonToSearch);
-                    System.out.println(planetToSearch);
-                }
-            } while((planetToSearch != null || moonToSearch != null) && !InputData.readYesOrNo(DO_YOU_WANT_TO_SEARCH_AGAIN));
-
-            if (planetToSearch == null) {
-                do {
-                    int id = Integer.parseInt(InputData.readString("Please enter an ID: ", true));
-                    String nome = InputData.readString("Please enter a name ", true);
-                    planetToSearch = Search.searchPlanet(id, nome);
-                } while((planetToSearch != null) && !InputData.readYesOrNo(DO_YOU_WANT_TO_SEARCH_AGAIN));
-            } else if (moonToSearch == null) {
-                do {
-                    int id = Integer.parseInt(InputData.readString("Please enter an ID: ", true));
-                    String nome = InputData.readString("Please enter a name ", true);
-                    moonToSearch = Search.searchMoon(id, nome);
-                } while((moonToSearch != null) && !InputData.readYesOrNo(DO_YOU_WANT_TO_SEARCH_AGAIN));
-            }
-
-            assert moonToSearch != null;
-            assert planetToSearch != null;
-
-            return buildPathFromTo(moonToSearch, planetToSearch);
+            return buildPathFromTo(moon, planet);
         } else if (InputData.readYesOrNo("Do you want to search planets or moons (Y for Planet and n for moon)?")) {
-            Planet planet1, planet2;
+            Planet planet1 = null, planet2 = null;
 
-            do {
-                int id1 = Integer.parseInt(InputData.readString("Please enter an ID: ", true));
-                String nome1 = InputData.readString("Please enter a name ", true);
-                planet1 = Search.searchPlanet(id1, nome1);
-            } while((planet1 != null) && !InputData.readYesOrNo(DO_YOU_WANT_TO_SEARCH_AGAIN));
+            planet1 = getPlanet(planet1);
 
-            do {
-                int id2 = Integer.parseInt(InputData.readString("Please enter an ID: ", true));
-                String nome2 = InputData.readString("Please enter a name ", true);
-                planet2 = Search.searchPlanet(id2, nome2);
-            } while((planet2 != null) && !InputData.readYesOrNo(DO_YOU_WANT_TO_SEARCH_AGAIN));
+            planet2 = getPlanet(planet2);
 
             assert planet1 != null;
             assert planet2 != null;
 
             return buildPathFromTo(planet1, planet2);
         } else {
-            Moon moon1, moon2;
+            Moon moon1 = null, moon2 = null;
 
-            do {
-                int id1 = Integer.parseInt(InputData.readString("Please enter an ID: ", true));
-                String nome1 = InputData.readString("Please enter a name ", true);
-                moon1 = Search.searchMoon(id1, nome1);
-            } while((moon1 != null) && !InputData.readYesOrNo(DO_YOU_WANT_TO_SEARCH_AGAIN));
+            moon1 = getMoon(moon1);
 
-            do {
-                int id2 = Integer.parseInt(InputData.readString("Please enter an ID: ", true));
-                String nome2 = InputData.readString("Please enter a name ", true);
-                moon2 = Search.searchMoon(id2, nome2);
-            } while((moon2 != null) && !InputData.readYesOrNo(DO_YOU_WANT_TO_SEARCH_AGAIN));
+            moon2 = getMoon(moon2);
 
             assert moon1 != null;
             assert moon2 != null;
